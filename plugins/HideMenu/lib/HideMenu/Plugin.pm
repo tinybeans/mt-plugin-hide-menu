3e6f17f0181fca1a601bdfbe13bc94ba52474301
tinybeans <register@tinybeans.net>
2012-07-11 11:18:29 +0900

    ウェブサイト、ブログでの非表示方法と、各スコープのプラグインの設定に対応させた

--- plugins/HideMenu/lib/HideMenu/Plugin.pm
+++ plugins/HideMenu/lib/HideMenu/Plugin.pm
@@ -6,13 +6,19 @@ sub _pre_run {
     my $app = MT->instance;
     my $core = MT->component( 'core' );
     my $cr = $core->registry( 'applications', 'cms', 'menus' );
-    my $hidemenu = $app->config( 'HideMenus' );
+    my $p = MT->component( 'HideMenu' );
+    my $hidemenu;
+    if ( $app->blog ) {
+        $hidemenu = $p->get_config_value( 'HideBlogMenuList', 'blog:' . $app->blog->id ) || $p->get_config_value( 'HideSystemMenuList', 'system' );
+    } else {
+        $hidemenu = $p->get_config_value( 'HideSystemMenuList', 'system' );
+    }
     my @memus = split( /,/, $hidemenu );
     for my $menu ( @memus ) {
         if (! $app->blog ) {
             $cr->{ $menu } = { view => [ 'blog' ] };
         } else {
-            $cr->{ $menu } = {};
+            $cr->{ $menu } = { view => [ 'system' ] };
         }
     }
     return 1;

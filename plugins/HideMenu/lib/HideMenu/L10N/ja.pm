a8d28770c43eba402ea2e9a844a8055bb82694c4
tinybeans <register@tinybeans.net>
2012-07-17 02:29:01 +0900

    システムのプラグインの設定の値の継承を明示的にした。

--- plugins/HideMenu/lib/HideMenu/L10N/ja.pm
+++ plugins/HideMenu/lib/HideMenu/L10N/ja.pm
@@ -5,6 +5,8 @@ use vars qw( %Lexicon );
 
 %Lexicon = (
     'Hide menu items.' => 'メニューを非表示にします。',
+    'Apply System Plugin Setting' => 'システムのプラグインの設定の値を適用する',
+    'If you are here is checked, the following settings will be ignored.' => 'ここがチェックされている場合、下記の設定は無視されます。',
 );
 
 1;
--- plugins/HideMenu/lib/HideMenu/Plugin.pm
+++ plugins/HideMenu/lib/HideMenu/Plugin.pm
@@ -8,8 +8,12 @@ sub _pre_run {
     my $cr = $core->registry( 'applications', 'cms', 'menus' );
     my $p = MT->component( 'HideMenu' );
     my $hidemenu;
-    if ( $app->blog ) {
-        $hidemenu = $p->get_config_value( 'HideBlogMenuList', 'blog:' . $app->blog->id ) || $p->get_config_value( 'HideSystemMenuList', 'system' );
+    my $blog_id = $app->blog ? $app->blog->id : 0;
+    if ( $blog_id ) {
+        $hidemenu = $p->get_config_value( 'HideBlogMenuList', 'blog:' . $blog_id );
+        if ($p->get_config_value( 'ApplySystemSetting', 'blog:' . $blog_id )) {
+            $hidemenu = $p->get_config_value( 'HideSystemMenuList', 'system' );
+        }
     } else {
         $hidemenu = $p->get_config_value( 'HideSystemMenuList', 'system' );
     }
